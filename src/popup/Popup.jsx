6635d388c463b9a94ad9e0171cb2dfcabdb1ea89
Andy Lindsay <andydlindsay@gmail.com>
2024-02-05 18:07:41 -0800

    initial commit

---
+++ src/popup/Popup.jsx
@@ -0,0 +1,13 @@
+import './Popup.scss';
+import icon from '../static/images/react-icon.png';
+
+const Popup = () => {
+  return (
+    <div className="Popup">
+      <h2>Hello World</h2>
+      <img src={icon} />
+    </div>
+  );
+};
+
+export default Popup;
